a91dee203e27a68a5c83863f6bcd22be157c61cc
Sandeep Ghemire <sandeep.ghemire@zohocorp.com>
2024-03-03 22:55:07 +0530

    First Commit

---
+++ src/FacebookStaticLoginPage/FacebookHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FacebookHeader = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Facebook
+      </h1>
+    </div>
+  )
+}
+
+export default FacebookHeader
